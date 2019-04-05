01dbe9e80a83d353e8371de24aedb100fc4a9337
Shubham Maurya <code.shaurya@gmail.com>
2019-04-05 18:56:42 +0530

    boundary fill comleted

--- Graphics/boundaryfill.c
+++ Graphics/boundaryfill.c
@@ -1,1 +1,32 @@
-boundaryfill.c
+
+#include <stdio.h>
+#include <graphics.h>
+
+void boundaryFill8(int x, int y, int fill_color,int boundary_color) 
+{ 
+	if(getpixel(x, y) != boundary_color && 
+	getpixel(x, y) != fill_color) 
+	{ 
+		putpixel(x, y, fill_color); 
+		boundaryFill8(x + 1, y, fill_color, boundary_color); 
+		boundaryFill8(x, y + 1, fill_color, boundary_color); 
+		boundaryFill8(x - 1, y, fill_color, boundary_color); 
+		boundaryFill8(x, y - 1, fill_color, boundary_color);
+	} 
+} 
+
+int main() 
+{
+	int gd = DETECT, gm; 
+	initgraph(&gd, &gm, ""); 
+	setcolor(4);
+	line(100, 30, 70, 70);
+	line(50, 70, 70, 70);
+	line(50, 70, 30, 150);
+	line(150, 150, 30, 150);
+	line(150, 150, 100, 30); 
+	boundaryFill8(100, 100, 2, 4); 
+	getch(); 
+	closegraph(); 
+	return 0; 
+} 
--- Graphics/floodfill.c
+++ Graphics/floodfill.c
@@ -12,20 +12,31 @@ void flood(int x, int y, int new_col)
 	} 
 } 
 
+void floodFill(int x, int y, int new_col, int old_col) 
+{ 
+	if (getpixel(x, y) == old_col) { 
+		putpixel(x, y, new_col); 
+		floodFill(x + 1, y, new_col, old_col); 
+		floodFill(x - 1, y, new_col, old_col); 
+		floodFill(x, y + 1, new_col, old_col); 
+		floodFill(x, y - 1, new_col, old_col); 
+	} 
+} 
+
 int main() 
 { 
 	int gd, gm = DETECT; 
 
 	initgraph(&gd, &gm, ""); 
-
+	setcolor(RED);
 	line(100, 30, 70, 70);
 	line(50, 70, 70, 70);
 	line(50, 70, 30, 150);
 	line(150, 150, 30, 150);
 	line(150, 150, 100, 30);
 
-	flood(100, 100, WHITE); 
-	flood(30, 30, RED); 
+	flood(100, 100, RED); 
+	floodFill(100, 100, GREEN, RED); 
 	getch(); 
 
 	return 0; 
